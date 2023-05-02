8f690a4af91fb849ed43420f47c1b2fefe55581f
Jeffrey Liu <jeffkliu@gmail.com>
2023-05-02 17:10:54 -0500

    Adding a Pause for Testing

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -9,13 +9,15 @@ function AlbumsList({ user }){
    const { data, error, isLoading } = useFetchAlbumsQuery(user);
    const [ addAlbum, results ] = useAddAlbumMutation(user);
 
+   //console.log(results);
+
    const handleAddAlbum = () => {
     addAlbum(user);
    };
 
     let content;
     if(isLoading){
-        content = <Skeleton times={3} />
+        content = <Skeleton className="h-10 w-full" times={3} />
     }else if(error){
         content = <div>Error loading albums.</div>
     }else{
@@ -29,9 +31,9 @@ function AlbumsList({ user }){
 
     return (
         <div>
-            <div>
-                Albums for {user.name}
-                <Button onClick={handleAddAlbum}>
+            <div className="m-2 flex flex-row items-center justify-between">
+                <h3 className="text-lg font-bold">Albums for {user.name}</h3>
+                <Button onClick={handleAddAlbum} loading={results.isLoading}>
                     + Add Album
                 </Button>
             </div>
--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -5,16 +5,10 @@ const fetchUsers = createAsyncThunk('users/fetch', async () => {
   const response = await axios.get('http://localhost:3005/users');
 
   // DEV ONLY!!!
-  await pause(1000);
+  //await pause(1000);
 
   return response.data;
 });
 
-// DEV ONLY!!!
-const pause = (duration) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, duration);
-  });
-};
 
 export { fetchUsers };
